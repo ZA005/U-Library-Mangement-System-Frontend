107d52011389c0b19b964a5300d790e3a40ff495
fesico <joemharijoy.fesico@unc.edu.ph>
2024-10-21 15:19:13 +0800

    login pop up

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+// screens/HomeScreen.tsx
+import React, { useState } from 'react';
 import { Container, Box, Typography, Button } from '@mui/material';
 import Header from '../components/Header/Header';
 import Carousel from '../components/Carousel/Carousel';
@@ -6,34 +7,59 @@ import ActionButtons from '../components/ActionButtons/ActionButtons';
 import Footer from '../components/Footer/Footer';
 import Line from '../components/Line/Line';
 import { useNavigate } from 'react-router-dom';
+import Login from "../components/LoginModal/LoginModal";
+
 
 const HomeScreen: React.FC = () => {
 
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+
   const handleLogin = () => {
-    navigate('/login');
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   const handleSignUp = () => {
-    navigate('register');
+    navigate('/register');
   };
 
   return (
     <Box display="flex" flexDirection="column" minHeight="100vh">
       <Container maxWidth="lg" sx={{ flexGrow: 1 }}>
-        <Header buttons={
-          <>
-            <Button color="inherit" onClick={handleSignUp}>Sign Up</Button>
-            <Button color="inherit" onClick={handleLogin}>Login</Button>
-          </>
-        }
+        <Header
+          buttons={
+            <>
+              <Button
+                color="inherit"
+                onClick={handleSignUp}
+                sx={{ color: '#d32f2f', border: '1px solid #d32f2f', marginRight: '10px' }}
+              >
+                Sign Up
+              </Button>
+              <Button
+                color="inherit"
+                onClick={handleLogin}
+                sx={{ color: '#d32f2f', border: '1px solid #d32f2f' }}
+              >
+                Login
+              </Button>
+            </>
+          }
         />
-          <Typography variant='h4'>Library Management System</Typography>
-          <Line/>
+        <Typography variant="h4">Library Management System</Typography>
+        <Line />
         <Carousel />
         <ActionButtons />
       </Container>
+
       <Footer />
+
+      {/* Login Modal */}
+      <Login open={open} onClose={handleClose} />
     </Box>
   );
 };
