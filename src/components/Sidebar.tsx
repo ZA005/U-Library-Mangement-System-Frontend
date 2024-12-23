0d6e2202b3172c33024fd39330fb6fa672866d0c
gd-salle <gerardojr.salle@gmail.com>
2024-12-23 12:35:03 +0800

    design: added MARC component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,71 @@
+// source code from https://mui.com/material-ui/react-drawer/
+import React from "react";
+import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Backdrop } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import HomeIcon from "@mui/icons-material/Home";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-start",
+}));
+
+const Sidebar: React.FC<{ open: boolean; onClose: () => void }> = ({
+  open,
+  onClose,
+}) => {
+  return (
+    <>
+      <Backdrop
+        open={open}
+        onClick={onClose}
+        sx={{ zIndex: (theme) => theme.zIndex.drawer - 1 }}
+      />
+      <Drawer
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: 240,
+            backgroundColor: "#282828",
+            color: "#FFF",
+            boxSizing: "border-box",
+            transition: (theme) =>
+              theme.transitions.create("transform", {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+              }),
+          },
+        }}
+        variant="persistent"
+        anchor="right"
+        open={open}
+      >
+        <DrawerHeader />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon sx={{ color: "#FFF" }} />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <LogoutIcon sx={{ color: "#FFF" }} />
+              </ListItemIcon>
+              <ListItemText primary="Logout" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
+  );
+};
+
+export default Sidebar;
