0b3175d9dca0e0e6d86170714283ce926858fa79
gd-salle <gerardojr.salle@gmail.com>
2025-02-18 17:58:52 +0800

    refactor: make footer and header responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,69 +1,71 @@
-import React from 'react';
-import { Box, Typography, Link } from '@mui/material';
-import Copyright from './Copyright';
-import UNCLogo from '../../assets/images/unc-logo.png';
-import YTLogo from '../../assets/svg/youtube.svg'
-import FBLogo from '../../assets/svg/facebook.svg'
-import IGLogo from '../../assets/svg/instagram.svg'
+import React from "react";
+import { Box, Typography, Link, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material";
+import Copyright from "./Copyright";
+import UNCLogo from "../../assets/images/unc-logo.png";
+import YTLogo from "../../assets/svg/youtube.svg";
+import FBLogo from "../../assets/svg/facebook.svg";
+import IGLogo from "../../assets/svg/instagram.svg";
 
 const Footer: React.FC = () => {
+  const theme = useTheme(); // Get the theme
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  if (isMobile) {
+    return <Copyright />;
+  }
+
   return (
-    <Box bgcolor="#3D3D3D" color="white">
-      <Box display="flex" justifyContent="space-evenly" alignItems="center" py={4}>
-
-        {/* LOGO ALIGNMENT */}
-        <Box display="flex" justifyContent="center" alignItems="center">
-          <img src={UNCLogo} alt="University Logo" height={60} />
+    <Box bgcolor="#3D3D3D" color="white" py={5}>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-evenly"
+        textAlign="center"
+        gap={3}
+        px={2}
+      >
+        {/* LOGO SECTION */}
+        <Box display="flex" alignItems="center">
+          <img src={UNCLogo} alt="University Logo" height={50} />
           <Box ml={2}>
-            <Typography>UNIVERSITY OF</Typography>
-            <Typography>NUEVA CACERES</Typography>
+            <Typography fontSize="16px" fontWeight="bold">
+              UNIVERSITY OF
+            </Typography>
+            <Typography fontSize="16px" fontWeight="bold">
+              NUEVA CACERES
+            </Typography>
           </Box>
         </Box>
 
-        {/* INFORMATION ALIGNMENT */}
-        <Box display="flex" justifyContent="center" alignItems="center">
-          <Box ml={2} display="flex" flexDirection="column" alignItems="center">
-            <Typography >J. Hernandez Avenue, Naga City 4404</Typography>
-            <Typography >09561301775 | 09071566898</Typography>
-            <Link href="mailto:info@unc.edu.ph" color="inherit">
-              info@unc.edu.ph
-            </Link>
-            <Link href="mailto:admission@unc.edu.ph" color="inherit">
-              admission@unc.edu.ph
-            </Link>
-          </Box>
+        {/* CONTACT INFORMATION */}
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Typography fontSize="14px">J. Hernandez Avenue, Naga City 4404</Typography>
+          <Typography fontSize="14px">09561301775 | 09071566898</Typography>
+          <Link href="mailto:info@unc.edu.ph" color="inherit" fontSize="14px">
+            info@unc.edu.ph
+          </Link>
+          <Link href="mailto:admission@unc.edu.ph" color="inherit" fontSize="14px">
+            admission@unc.edu.ph
+          </Link>
         </Box>
 
-        {/* ICON ALIGNMENT */}
-        <Box display="flex" justifyContent="center" alignItems="center" gap={2}>
-          <Link href="https://www.youtube.com/@UniversityofNuevaCaceres" target="_blank" color="inherit">
-            <img
-              src={YTLogo}
-              alt="YouTube"
-              height={50}
-              width={50}
-            />
+        {/* SOCIAL MEDIA ICONS */}
+        <Box display="flex" gap={2}>
+          <Link href="https://www.youtube.com/@UniversityofNuevaCaceres" target="_blank">
+            <img src={YTLogo} alt="YouTube" height={40} width={40} />
           </Link>
-
-          <Link href="https://www.facebook.com/UniversityOfNuevaCaceres" target="_blank" color="inherit">
-            <img
-              src={FBLogo}
-              alt="Facebook"
-              height={40}
-              width={40}
-            />
+          <Link href="https://www.facebook.com/UniversityOfNuevaCaceres" target="_blank">
+            <img src={FBLogo} alt="Facebook" height={35} width={35} />
           </Link>
-
-          <Link href="https://www.instagram.com/uncgreyhounds/" target="_blank" color="inherit">
-            <img
-              src={IGLogo}
-              alt="Instagram"
-              height={50}
-              width={50}
-            />
+          <Link href="https://www.instagram.com/uncgreyhounds/" target="_blank">
+            <img src={IGLogo} alt="Instagram" height={40} width={40} />
           </Link>
         </Box>
       </Box>
+
+      {/* COPYRIGHT */}
       <Copyright />
     </Box>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,49 +1,75 @@
-import React from 'react';
-import { AppBar, Toolbar, Box, Typography } from '@mui/material';
-import Style from './Header.module.css';
-import LibraryLogo from '../../assets/images/lms-logo.png'
-import { Helmet } from 'react-helmet';
-import eliblogo from '../../assets/images/lms-logo.png'
-// Passing of props so that the buttons can be dynamic
+import React, { useState } from "react";
+import { AppBar, Toolbar, Box, Typography, IconButton, Drawer } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Style from "./Header.module.css";
+import LibraryLogo from "../../assets/images/lms-logo.png";
+import { Helmet } from "react-helmet";
+import eliblogo from "../../assets/images/lms-logo.png";
+
 interface HeaderProps {
   buttons?: React.ReactNode;
   title?: React.ReactNode;
 }
 
 const Header: React.FC<HeaderProps> = ({ buttons, title }) => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
   return (
     <>
       <Helmet>
         <title>{title}</title>
-        <meta charSet='utf-8' />
-        <link rel='icon' type='image/png' href={eliblogo} />
+        <meta charSet="utf-8" />
+        <link rel="icon" type="image/png" href={eliblogo} />
       </Helmet>
-      <AppBar position="static" color="transparent" elevation={0} className={Style.bottomLine}>
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
+      <AppBar
+        position="static"
+        color="transparent"
+        elevation={0}
+        className={Style.bottomLine}
+      >
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          {/* Left Section - Logo & Title */}
           <Box display="flex" alignItems="center">
-            <img src={LibraryLogo} alt="Library Logo" height={50} />
-            <Box display="flex" flexDirection="column">
+            <img src={LibraryLogo} alt="Library Logo" height={40} />
+            <Box ml={2} display="flex" flexDirection="column">
               <Typography
                 variant="caption"
-                ml={2}
-                fontSize="15px"
+                fontSize="14px"
                 fontWeight="bold"
                 className={Style.spartan}
-                sx={{ fontFamily: 'Spartan, sans-serif !important' }} // this line overrides the default fontfamily of MUI fontfamily
+                sx={{ fontFamily: "Spartan, sans-serif !important" }}
               >
                 <span className={Style.title}>A</span>CQUIRE
               </Typography>
-              <Typography variant="caption" ml={2}>
+              <Typography variant="caption" fontSize="12px">
                 Library Management System
               </Typography>
             </Box>
           </Box>
-          {/* Render buttons passed as props */}
-          <Box>{buttons}</Box>
+
+          {/* Right Section - Buttons */}
+          <Box sx={{ display: { xs: "none", md: "block" } }}>{buttons}</Box>
+
+          {/* Mobile Menu Icon */}
+          <IconButton
+            sx={{ display: { xs: "block", md: "none" } }}
+            onClick={() => setMobileOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
-    </>
 
+      {/* Mobile Drawer for Buttons */}
+      <Drawer
+        anchor="right"
+        open={mobileOpen}
+        onClose={() => setMobileOpen(false)}
+        sx={{ "& .MuiDrawer-paper": { width: 250, padding: 2 } }}
+      >
+        {buttons}
+      </Drawer>
+    </>
   );
 };
 
