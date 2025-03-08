ab715354bf280a9e11c32bb0bfb2a807f5b63d75
gd-salle <gerardojr.salle@gmail.com>
2025-03-08 12:20:13 +0800

    update: added fallback when page is loading

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,16 +1,32 @@
-import loadable from "@loadable/component"
+import loadable from "@loadable/component";
+import LoadingSpinner from "../components/LoadingSpinner";
 
-export const HomeScreen = loadable(() => import("./HomeScreen"));
-export const PageNotFound = loadable(() => import("./PageNotFound"))
-export const ActivateUser = loadable(() => import("./AccountActivation/ActivateUser"))
-export const AccountLibraryCard = loadable(() => import("./AccountActivation/AccountLibraryCard"))
+export const HomeScreen = loadable(() => import("./HomeScreen"), {
+    fallback: LoadingSpinner(),
+});
 
+export const PageNotFound = loadable(() => import("./PageNotFound"), {
+    fallback: LoadingSpinner(),
+});
+export const ActivateUser = loadable(() => import("./AccountActivation/ActivateUser"), {
+    fallback: LoadingSpinner(),
+});
+export const AccountLibraryCard = loadable(() => import("./AccountActivation/AccountLibraryCard"), {
+    fallback: LoadingSpinner(),
+});
 
-
-export const AccessionRecord = loadable(() => import("./Acquisition/AccessionRecord"))
-export const Browse = loadable(() => import("./Browse"))
-export const TestingPage = loadable(() => import("./TestPage"))
-export const Catalog = loadable(() => import("./Catalog"))
+export const AccessionRecord = loadable(() => import("./Acquisition/AccessionRecord"), {
+    fallback: LoadingSpinner(),
+});
+export const Browse = loadable(() => import("./Browse"), {
+    fallback: LoadingSpinner(),
+});
+export const TestingPage = loadable(() => import("./TestPage"), {
+    fallback: LoadingSpinner(),
+});
+export const Catalog = loadable(() => import("./Catalog"), {
+    fallback: LoadingSpinner(),
+});
 // export const = loadable(() => import("./"))
 // export const = loadable(() => import("./"))
 // export const = loadable(() => import("./"))
