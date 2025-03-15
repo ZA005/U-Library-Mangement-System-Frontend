1f00ce36d4e7f205056d834bd300323df39f4a96
gd-salle <gerardojr.salle@gmail.com>
2025-03-15 13:51:23 +0800

    update: make the button responsive

--- src/components/SelectMenu/index.tsx
+++ src/components/SelectMenu/index.tsx
@@ -3,14 +3,22 @@ import { FormControl, Select, MenuItem, InputLabel, SelectChangeEvent } from "@m
 
 interface SelectMenuProps {
     label: string;
-    value: string;
-    onChange: (event: SelectChangeEvent<string>) => void;
-    options: { id: string; name: string }[];
+    value: string | number;
+    onChange: (event: SelectChangeEvent<string | number>) => void;
+    options?: { id: string | number; name: string }[];
+    disabled?: boolean;
 }
 
-const SelectMenu: React.FC<SelectMenuProps> = ({ label, value, onChange, options }) => {
+const SelectMenu: React.FC<SelectMenuProps> = ({ label, value, onChange, options = [], disabled = false }) => {
     return (
-        <FormControl fullWidth variant="outlined" size="small">
+        <FormControl
+            variant="outlined"
+            size="small"
+            disabled={disabled}
+            sx={{
+                width: { xs: "100%", md: "350px" },
+            }}
+        >
             <InputLabel id={`${label}-label`}>{label}</InputLabel>
             <Select
                 labelId={`${label}-label`}
@@ -18,11 +26,15 @@ const SelectMenu: React.FC<SelectMenuProps> = ({ label, value, onChange, options
                 onChange={onChange}
                 label={label}
             >
-                {options.map((option) => (
-                    <MenuItem key={option.id} value={option.id}>
-                        {option.name}
-                    </MenuItem>
-                ))}
+                {options.length === 0 ? (
+                    <MenuItem disabled>No options available</MenuItem>
+                ) : (
+                    options.map((option) => (
+                        <MenuItem key={option.id} value={option.id}>
+                            {option.name}
+                        </MenuItem>
+                    ))
+                )}
             </Select>
         </FormControl>
     );
