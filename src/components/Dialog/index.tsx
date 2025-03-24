68003d3ce1aa4916a86864bf056845de700f5bb4
gd-salle <gerardojr.salle@gmail.com>
2025-03-24 14:08:55 +0800

    update: moved Dialog component inside component folder

---
+++ src/components/Dialog/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Dialog, DialogTitle, DialogActions, DialogContent, IconButton, Button, Box } from "@mui/material";
+
+interface BookReferenceDialogProps {
+    open: boolean;
+    title: string;
+    onClose: () => void;
+    iconButtons?: React.ReactNode[];
+    content: React.ReactNode;
+    actions?: React.ReactNode;
+}
+
+const DynamicDialog: React.FC<BookReferenceDialogProps> = ({ open, title, onClose, iconButtons = [], content, actions }) => {
+    return (
+        <Dialog open={open} fullWidth maxWidth="md" onClose={onClose}>
+            <DialogTitle display="flex" alignItems="center" justifyContent="space-between">
+                {title}
+                {iconButtons.length > 0 && (
+                    <Box display="flex" alignItems="center" gap={3} flexDirection="row">
+                        {iconButtons.map((icon, index) => (
+                            <IconButton key={index} sx={{ p: 0 }}>
+                                {icon}
+                            </IconButton>
+                        ))}
+                    </Box>
+                )}
+            </DialogTitle>
+            <DialogContent dividers sx={{ minHeight: "300px", maxHeight: "500px", overflow: "auto" }}>
+                {content}
+            </DialogContent>
+            <DialogActions>
+                {actions}
+                <Button onClick={onClose} variant="text" size="small" sx={{ color: "#d32f2f" }}>
+                    Close
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default DynamicDialog;
